15bc455a3ef331a10e3007fa448085a672fc28dd
Anderson Silva <andersonsilvadev01@gmail.com>
2024-05-09 19:18:49 -0300

    criando arquivo da pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  projectId: '2h8k27',
+  projectId: 'f5kcw6',
   e2e: {
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1440,
@@ -9,7 +9,7 @@ module.exports = {
       reportDir: 'mochawesome-report',
       overwrite: false,
       html: true,
-      json: true
+      json: false
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
