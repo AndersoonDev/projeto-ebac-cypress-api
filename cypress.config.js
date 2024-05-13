97445a23d888132a778bb655cf474ab5891c2c6b
Anderson Silva <andersonsilvadev01@gmail.com>
2024-05-13 12:47:27 -0300

    corrigindo testes

--- cypress.config.js
+++ cypress.config.js
@@ -2,15 +2,15 @@ module.exports = {
   projectId: 'f5kcw6',
   e2e: {
     baseUrl: 'http://localhost:3000',
-    viewportWidth: 1440,
-    viewportHeight: 900,
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: 'mochawesome-report',
-      overwrite: false,
-      html: false,
-      json: false
-    },
+    // viewportWidth: 1440,
+    // viewportHeight: 900,
+    // reporter: 'mochawesome',
+    // reporterOptions: {
+    //   reportDir: 'mochawesome-report',
+    //   overwrite: false,
+    //   html: false,
+    //   json: false
+    // },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
